4d46eecf8fd05bb2196263a7085500d1ff1eb0f1
Deline Neo <delineneo@gmail.com>
2016-01-28 21:45:24 +1100

    Initial Commit

---
+++ WireMockClient/WireMockClient.h
@@ -0,0 +1,19 @@
+//
+//  WireMockClient.h
+//  WireMockClient
+//
+//  Created by Deline Neo on 28/01/2016.
+//  Copyright © 2016 Deline Neo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WireMockClient.
+FOUNDATION_EXPORT double WireMockClientVersionNumber;
+
+//! Project version string for WireMockClient.
+FOUNDATION_EXPORT const unsigned char WireMockClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WireMockClient/PublicHeader.h>
+
+
